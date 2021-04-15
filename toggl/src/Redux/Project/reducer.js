98547e628e492905fd9629d6833cecbf9a409eea
khushbookhator <khushbookhator11@gmail.com>
2021-04-15 12:02:02 +0530

    projectPage

--- toggl/src/Redux/Project/reducer.js
+++ toggl/src/Redux/Project/reducer.js
@@ -1,25 +1,48 @@
-import { POST_PROJECT_FAILURE, POST_PROJECT_REQ, POST_PROJECT_SUCCESS } from "./actiontype"
+import { GET_PROJECT_FAILURE, POST_PROJECT_FAILURE, POST_PROJECT_REQ, POST_PROJECT_SUCCESS } from "./actiontype"
 
 
 const init = {
-    data: []
+    project: [],
+    isPosting: false
 }
 const projectReducer = (state = init, action) => {
     switch(action.type){
         case POST_PROJECT_SUCCESS: {
             return{
                 ...state,
-                data: action.payload
+                project: action.project,
+                isPosting: false
             }
         }
         case POST_PROJECT_REQ: {
             return{
-                ...state
+                ...state,
+                isPosting: true
             }
         }
         case POST_PROJECT_FAILURE: {
             return{
-
+                ...state,
+                isPosting: false
+            }
+        }
+        case GET_PROJECT_REQ: {
+            return{
+                ...state,
+                isPosting: true
+            }
+        }
+        case GET_PROJECT_SUCCESS: {
+            return{
+                ...state,
+                project: action.project,
+                isPosting: false
+            }
+        }
+        case GET_PROJECT_FAILURE: {
+            return{
+                ...state,
+                isPosting: false
             }
         }
         default:
