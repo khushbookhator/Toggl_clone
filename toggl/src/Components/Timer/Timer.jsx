f02f4200ba8bb1f7fc17136139730899e574f799
ABHISHEK <abhi2iet@gmail.com>
2021-04-16 23:32:16 +0530

    timer ui final

--- toggl/src/Components/Timer/Timer.jsx
+++ toggl/src/Components/Timer/Timer.jsx
@@ -168,7 +168,7 @@ export const Timer=()=>{
             </div>
             <div className={timerstyles.dayPart}>
                 <h5 style={{marginLeft:"20px", fontSize:"15px", fontWeight:"500"}}>Today</h5>
-                <p style={{marginRight:"40px"}}>
+                <p style={{marginRight:"140px", fontWeight:"bold"}}>
                     {
                         getTime(task.reduce((acc, b) => acc + b.total_time, 0))
                     }
